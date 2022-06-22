4bbb2cefefd240f45ca8d15dde22ef0052ca3770
Polismun <90210547+Polismun@users.noreply.github.com>
2022-06-22 15:40:26 +0200

    Add files via upload
    true

--- js/Factory/VideoFactory.js
+++ js/Factory/VideoFactory.js
@@ -5,9 +5,9 @@ export default class VideoFactory {
 
     createHTML(element) {
         let eltVideo = document.createElement('video');
-        eltVideo.setAttribute("controls", "controls")
         eltVideo.setAttribute('src', element.video);
         eltVideo.setAttribute('role', 'button');
+        eltVideo.setAttribute('button',true);
         eltVideo.className = 'ph-media';
 
         return eltVideo;
